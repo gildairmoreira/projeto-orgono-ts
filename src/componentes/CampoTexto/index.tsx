ea1568c248e5441ab7bd3e43ffeb327af116be1e
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-03-10 22:14:49 -0300

    nova parte de data de entrada

--- src/componentes/CampoTexto/index.tsx
+++ src/componentes/CampoTexto/index.tsx
@@ -8,9 +8,11 @@ interface CampoTextoProps
     label: string
     valor: string
     obrigatorio?: boolean
+    tipo?: 'text' | 'passoword' | 'date' | 'email' | 'number'
 }
 
-const CampoTexto = ({ aoAlterado, label, placeholder, valor, obrigatorio = false}: CampoTextoProps) =>
+const CampoTexto = ({ aoAlterado, label, placeholder, valor, obrigatorio = false 
+, tipo='text'}: CampoTextoProps) =>
 {
 
     const placeholderModificada = `${placeholder}...`
@@ -30,6 +32,7 @@ const CampoTexto = ({ aoAlterado, label, placeholder, valor, obrigatorio = false
                 onChange={aoDigitado}
                 required={obrigatorio}
                 placeholder={placeholderModificada}
+                type={tipo}            
             />
         </div>
     )
